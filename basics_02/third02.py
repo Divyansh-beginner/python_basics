ca103fb5f4f3c48fe6cae35bc5bb77641d55a61d
divya_singh <divya_singh@gmail.com>
2025-09-13 23:23:58 +0530

    6th commit: 9th vid done. strings and lists.

--- basics_02/third02.py
+++ basics_02/third02.py
@@ -14,8 +14,48 @@ print(random.randint.__le__)
 print(0.1+0.1+0.1-0.3)
 # print(2**10000)
 x = 10
-print(dir(x))
-print(True + 4) #gives 5
+# print(dir(x))
+# print(True + 4) #gives 5
+print({1,2,3,4}) #its a set , braces without key:value pair is set(from maths).
+x = "astring"
+# print(dir(x))
+# print(help(x.__class__))
+print(x[0])
+# print(help(x.lstrip))
+# print(help(x.rstrip))
+# print(help(x.strip))
+# print(help(x.__getitem__))
+# print(help(x.removeprefix))
+# print(help(x.rjust))
+# print(help(x.split))
+# print(help(x.__reduce__))
+# print(help(x.__str__))
+# print(help(x.__getitem__))
+# print(help(slice))
+# print(x.__getitem__(slice(0,4,None)))
+# print(help(x.replace))
+# print(x.replace("tri","pro",-1))
+# print(help(x.encode))
+# print(help(x.format))
+# print(help(x.maketrans))
+# print(help(x.translate))
+# print(help(x.split))
+y = "could {} be added later as a {} ?"
+print(y.format("this","variable"))
+y = ["string","anotherstring"]
+# print(dir(y))
+# print(help(y.__getitem__))
+print(y.__getitem__(slice(1,None,None)))
+# print(help(x.join))
+print(r"hello\nmybrothe")
+print(r"c\user\\")
+
+
+
+
+
+
+
 
 
 
