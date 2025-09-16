164e06962c707d90d2e1f1095e112c0ed09fcb28
Divyansh-beginner <divyanshubabu2003@gmail.com>
2025-09-16 22:29:10 +0530

    8th commit: solving loops questions

---
+++ practice_02/loops_03.py
@@ -0,0 +1,4 @@
+n: int = int(input("enter the number:"))
+for i in range(1,11):
+    if(i!=5):
+        print(f"{n} X {i} = {n*i}")
---
+++ practice_02/loops_05.py
@@ -0,0 +1,16 @@
+input_str:str = input("enter the string: ")
+flag: bool = True
+flag2: bool = True
+for i in range(0 , len(input_str)-1):
+    if input_str[i] != input_str[i+1] :
+        if flag :
+            print(input_str[i])
+            flag2 = False
+            break
+        else :
+            flag = True
+    else :
+        flag = False
+if flag2 :
+    print("all characters are repeating") 
+    
