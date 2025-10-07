67e4a5cc409ff56d8469507843c30efbe45cc4ab
Divyansh-beginner <divyanshubabu2003@gmail.com>
2025-10-08 03:26:42 +0530

    16th commit: learning about apis, networking. sockets are done.

---
+++ api_and_networking/sockets/server.py
@@ -0,0 +1,29 @@
+import socket
+import json 
+import os
+
+def get_file_name_dir(data):
+    start = data.find("*")+1
+    end = data.rfind("*")
+    filename = data.__getitem__(slice(start , end , 1))
+    directory = None
+    for root, dirs, files in os.walk(r"C:\Users\divya"):
+        if filename in files:
+            directory = os.path.join(root, filename)
+            break
+    return directory
+
+def main():    
+    s = socket.socket()
+    s.bind(('0.0.0.0', 62055))
+    s.listen()
+    while(True):
+        new_conn , addr = s.accept()
+        data = new_conn.recv(1024).decode()
+        print(data)
+        file_name_dir = get_file_name_dir(data)
+        with open(file_name_dir,"r",encoding="utf-8",errors="ignore") as data_to_send:
+            new_conn.send(json.dumps(data_to_send.read()).encode())
+        new_conn.close()
+
+if __name__ == "__main__":main()
